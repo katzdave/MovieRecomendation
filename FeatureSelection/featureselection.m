d37b3fc3a6108f45bd72a292e3f92054ee453bee
Chris Curro <curro@cooper.edu>
2015-05-11 10:44:42 -0400

    features out to csv from matlab

--- FeatureSelection/featureselection.m
+++ FeatureSelection/featureselection.m
@@ -48,25 +48,55 @@ for ii = 1:(nmt/nm)
     end
 end
 
-featnames = tag{2}(ids(1:10)+1);
-feats = rand(nm,10);
-tmp = double(m(:,1));
-feats = [tmp feats];
+%% Write features out to csv file
 
-outp = fopen('outp.txt','w');
-for ii=1:length(featnames)
-    if(ii < length(featnames))
-        fprintf(outp,'%s~',featnames{ii});
-    else
-        fprintf(outp,'%s\n',featnames{ii});
+movieIDs = containers.Map({0},{0});
+
+count = 1;
+for ii = 1:size(mt,1)
+    ID = mt(ii,1);
+    if isKey(movieIDs,ID) == 0 
+       movieIDs = [movieIDs; containers.Map({ID},{count})];
+       count = count + 1
     end
 end
 
-for ii=1:nm
-    fprintf(outp,'%d',feats(ii,1));
-    for jj=2:size(feats,2)
-       fprintf(outp,'~%f',feats(ii,jj)); 
-    end
-    fprintf(outp,'\n');
+features = zeros(9734,1128);
+
+h = waitbar(0,'Grab a Coffee')
+
+for ii = 1:size(mt,1) 
+    movieId = movieIDs(mt(ii,1));
+    tagId = mt(ii,2) + 1;
+    tagW = mt(ii,3); % tag weight/relevance
+    features(movieId,tagId) = tagW;
+    waitbar(ii/size(mt,1),h,sprintf('%f',ii/size(mt,1)))
 end
-fclose(outp);
+
+clearvars -except features
+csvwrite('features.csv',features)
+
+%%
+
+% featnames = tag{2}(ids(1:10)+1);
+% feats = rand(nm,10);
+% tmp = double(m(:,1));
+% feats = [tmp feats];
+% 
+% outp = fopen('outp.txt','w');
+% for ii=1:length(featnames)
+%     if(ii < length(featnames))
+%         fprintf(outp,'%s~',featnames{ii});
+%     else
+%         fprintf(outp,'%s\n',featnames{ii});
+%     end
+% end
+% 
+% for ii=1:nm
+%     fprintf(outp,'%d',feats(ii,1));
+%     for jj=2:size(feats,2)
+%        fprintf(outp,'~%f',feats(ii,jj)); 
+%     end
+%     fprintf(outp,'\n');
+% end
+% fclose(outp);
